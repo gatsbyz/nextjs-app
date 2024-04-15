53e72a6643c1ac34cb4a4e6d353f9178f5921d33
Jesse Lee <gatsby1118@gmail.com>
2024-04-14 20:34:52 -0400

    add header

---
+++ app/header.tsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import { Container, Anchor, Group, Burger, Box, Image } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { MantineLogo } from '@mantinex/mantine-logo';
+import classes from './header.module.css';
+
+const userLinks = [
+  { link: '#', label: 'Contact Us' },
+//   { link: '#', label: 'Account settings' },
+//   { link: '#', label: 'Support options' },
+];
+
+const mainLinks = [
+  { link: '#', label: 'Our Mission' },
+  { link: '#', label: 'Services' },
+];
+
+export function Header() {
+  const [opened, { toggle }] = useDisclosure(false);
+  const [active, setActive] = useState(0);
+
+  const mainItems = mainLinks.map((item, index) => (
+    <Anchor<'a'>
+      href={item.link}
+      key={item.label}
+      className={classes.mainLink}
+      data-active={index === active || undefined}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(index);
+      }}
+    >
+      {item.label}
+    </Anchor>
+  ));
+
+  const secondaryItems = userLinks.map((item) => (
+    <Anchor
+      href={item.link}
+      key={item.label}
+      onClick={(event) => event.preventDefault()}
+      className={classes.secondaryLink}
+    >
+      {item.label}
+    </Anchor>
+  ));
+
+  return (
+    <header className={classes.header}>
+      <Container className={classes.inner}>
+      <Image src="/logo.png" alt="Handy & Dandy Logo" w={70} h={70} />
+        <Box className={classes.links} visibleFrom="sm">
+          <Group justify="flex-end">{secondaryItems}</Group>
+          <Group gap={0} justify="flex-end" className={classes.mainLinks}>
+            {mainItems}
+          </Group>
+        </Box>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          className={classes.burger}
+          size="sm"
+          hiddenFrom="sm"
+        />
+      </Container>
+    </header>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react';
 import ServicesPage from './services';
 import { IconBrandTelegram, IconBrandWhatsapp, IconPhone, IconExternalLink } from '@tabler/icons-react';
 import { Footer } from './footer';
+import { Header } from './header';
 
 const TypingEffect = ({
   words = [""],
@@ -76,15 +77,14 @@ export default function HomePage() {
   
   return (
     <Container size="lg" px="md">
+      <Header></Header>
       <Stack gap="lg" align="center">
         <Box w="100%" p='20px 0' ta='center'>
-          <Image src="/logo.png" alt="Handy & Dandy Logo" w={150} h={150} mx="auto" mb="xl"/>
+          {/* <Image src="/logo.png" alt="Handy & Dandy Logo" w={150} h={150} mx="auto" mb="xl"/> */}
 
-          <Stack mx="auto" justify="left">
-            <Text size="xl" fw={500}>
-            ✅ We are fully insured. Request for a COI.
-            </Text>
-          </Stack>
+          <Text size="xl" fw={500} >
+            We Serve Homes and Businesses in <TypingEffect words={['NYC', 'Brooklyn', 'Queens']} typingSpeed={120} deletingSpeed={120} delayBetweenWords={1000} />
+          </Text>
 
           <Button 
             my="xl"
@@ -97,10 +97,6 @@ export default function HomePage() {
           >
             Check out our portfolio on Thumbtack
           </Button>
-
-          <Text size="xl" fw={500} mb="xl">
-            We Serve Homes and Businesses in <TypingEffect words={['NYC', 'Brooklyn', 'Queens']} typingSpeed={120} deletingSpeed={120} delayBetweenWords={1000} />
-          </Text>
 
           <Text size="xl" fw={500} my="xl">
             ⭐ Our Mission ⭐
@@ -139,27 +135,12 @@ export default function HomePage() {
               </Button>
             )}
           </SimpleGrid>
-          {/* <Group grow wrap="nowrap">
-            <Button variant="outline" component="a" href="https://t.me/gatsbeta" target="_blank" leftSection={<IconBrandTelegram size={18} />}>
-              Telegram
-            </Button>
-            <Button variant="outline" component="a" href={`https://wa.me/4048404655?text=${whatsappMessage}`} target="_blank" leftSection={<IconBrandWhatsapp size={18} />}>
-              WhatsApp
-            </Button>
-            {showPhoneNumber ? (
-              <Text>
-                <a href={`tel:${phoneNumber}`} style={{ textDecoration: "none", color: theme.colors.blue[6] }}>
-                  {phoneNumber}
-                </a>
-              </Text>
-            ) : (
-              <Button variant="outline" onClick={handleShowPhoneNumber} leftSection={<IconPhone size={18} />}>
-                Show Phone Number
-              </Button>
-            )}
-          </Group> */}
         </Box>
+        <Text size="xl" fw={500}>
+            ✅ Fully insured. Request for a COI.
+        </Text>
       </Stack>
+      
       <Footer></Footer>
     </Container>
   );
